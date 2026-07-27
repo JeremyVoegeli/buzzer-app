1e2cf5daa50449cca7db8d255a4fe18048b125c5
Jeremy Voegeli <jeremy.voegeli@uconn.edu>
2026-07-27 16:29:28 -0400

    updated temp starter code in create_new_user.py

--- lambdas/create_new_user.py
+++ lambdas/create_new_user.py
@@ -1,1 +1,10 @@
-#temp file, getting terraform wired up first
+import json
+
+def lambda_handler(event, context):
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "This is the message.",
+            "status": "success"
+        })
+    }
