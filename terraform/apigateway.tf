c68bc8138623feb362de8c86a544f605181c67b0
Jeremy Voegeli <jeremy.voegeli@uconn.edu>
2026-07-28 17:05:53 -0400

    creating production stage for api gateway:

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -21,3 +21,9 @@ resource "aws_apigatewayv2_deployment" "websocket_api_deployment" {
     api_id = aws_apigatewayv2_api.websocket_api.id
     depends_on = [aws_apigatewayv2_route.create_user_route] #<- says, "don't create this resource until create_user_route is created"
 }
+
+resource "aws_apigatewayv2_stage" "websocket_api_stage" {
+    api_id = aws_apigatewayv2_api.websocket_api.id
+    name = "prod"
+    deployment_id = aws_apigatewayv2_deployment.websocket_api_deployment.id
+}
