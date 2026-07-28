ee6ece21f1b86dc18a094d5859c5ae57f908bffd
Jeremy Voegeli <jeremy.voegeli@uconn.edu>
2026-07-28 16:59:58 -0400

    creating initial api gateway terraform file

---
+++ terraform/apigateway.tf
@@ -0,0 +1,18 @@
+resource "aws_apigatewayv2_api" "websocket_api" {
+    name = "websocket_api"
+    protocol_type = "WEBSOCKET"
+    route_selection_expression = "$request.body.action"
+}
+
+resource "aws_apigatewayv2_integration" "api_integration" {
+    api_id = aws_apigatewayv2_api.websocket_api.id
+    integration_type = "AWS_PROXY"
+    integration_uri = aws_lambda_function.create_new_user_lambda.invoke_arn
+    integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "create_user_route" {
+    api_id = aws_apigatewayv2_api.websocket_api.id
+    route_key = "create_user"
+    target = "integrations/${aws_apigatewayv2_integration.api_integration.id}"
+}
