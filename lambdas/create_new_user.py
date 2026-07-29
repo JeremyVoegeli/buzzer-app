c5ff7f9cd3ae4b5e35ced7a54c1ba9f67738d145
Jeremy Voegeli <jeremy.voegeli@uconn.edu>
2026-07-29 10:16:26 -0400

    modifying lambda to be able to send response back to user through api gateway

--- lambdas/create_new_user.py
+++ lambdas/create_new_user.py
@@ -1,10 +1,22 @@
 import json
+import boto3
 
 def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "This is the message.",
-            "status": "success"
-        })
+    connection_id = event["requestContext"]["connectionId"]
+    domain = event["requestContext"]["domainName"]
+    stage = event["requestContext"]["stage"]
+    endpoint_url = f"https://{domain}/{stage}"
+
+    client = boto3.client("apigatewaymanagementapi", endpoinet_url=endpoint_url)
+
+    response_data = {
+        "message": "This is a message.",
+        "status": "success"
     }
+
+    client.post_to_connection(
+        ConnectionId=connection_id,
+        Data=json.dumps(response_data)
+    )
+
+    return {"statusCode": 200,}
