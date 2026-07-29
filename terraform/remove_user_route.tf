f97794f14aef741627a41141d1703bcf63cbd5c0
Jeremy Voegeli <jeremy.voegeli@uconn.edu>
2026-07-29 12:26:07 -0400

    cleaning up terraform renaming issue

--- terraform/remove_user_route.tf
+++ terraform/remove_user_route.tf
@@ -2,7 +2,7 @@ resource "aws_apigatewayv2_integration" "remove_user_integration" {
     api_id = aws_apigatewayv2_api.websocket_api.id
     integration_type = "AWS_PROXY"
     integration_uri = aws_lambda_function.remove_user_lambda.invoke_arn
-    integration_method = "DELETE"
+    integration_method = "POST"
 }
 
 resource "aws_apigatewayv2_route" "remove_user_route" {
