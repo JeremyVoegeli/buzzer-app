31d13e1aa99f4b0992355939763bb2eca6b82bd2
Jeremy Voegeli <jeremy.voegeli@uconn.edu>
2026-07-29 12:11:05 -0400

    refactoring tf apigateway file structure

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -4,19 +4,6 @@ resource "aws_apigatewayv2_api" "websocket_api" {
     route_selection_expression = "$request.body.action"
 }
 
-resource "aws_apigatewayv2_integration" "api_integration" {
-    api_id = aws_apigatewayv2_api.websocket_api.id
-    integration_type = "AWS_PROXY"
-    integration_uri = aws_lambda_function.create_new_user_lambda.invoke_arn
-    integration_method = "POST"
-}
-
-resource "aws_apigatewayv2_route" "create_user_route" {
-    api_id = aws_apigatewayv2_api.websocket_api.id
-    route_key = "create_user"
-    target = "integrations/${aws_apigatewayv2_integration.api_integration.id}"
-}
-
 resource "aws_apigatewayv2_deployment" "websocket_api_deployment" {
     api_id = aws_apigatewayv2_api.websocket_api.id
     depends_on = [aws_apigatewayv2_route.create_user_route] #<- says, "don't create this resource until create_user_route is created"
