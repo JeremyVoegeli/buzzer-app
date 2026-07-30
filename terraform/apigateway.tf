fd5237f64c039569ab6fa0e16a78ffe36a35fdf9
Jeremy Voegeli <jeremy.voegeli@uconn.edu>
2026-07-30 12:23:39 -0400

    adding lifecycle block to api gateway deployment

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -8,10 +8,13 @@ resource "aws_apigatewayv2_deployment" "websocket_api_deployment" {
     api_id = aws_apigatewayv2_api.websocket_api.id
     triggers = {
         redeployment = join(",", [
-            aws_apigatewayv2_route.create_new_user_route.id,
+            aws_apigatewayv2_route.create_user_route.id,
             aws_apigatewayv2_route.remove_user_route.id,
             aws_apigatewayv2_route.clear_buzzes_route.id
         ])
+    }
+    lifecycle {
+        create_before_destroy = true
     }
 }
 
