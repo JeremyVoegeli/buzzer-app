d6c06f8a94175282e2b1913919f6de56e4694335
Jeremy Voegeli <jeremy.voegeli@uconn.edu>
2026-08-10 18:10:51 -0400

    first attempt at buzz.py

--- lambdas/buzz.py
+++ lambdas/buzz.py
@@ -1,22 +1,89 @@
 import json
 import boto3
+from boto3.dynamodb.conditions import Key
+from datetime import datetime, timezone
+
+"""
+Expected Request Format:
+{
+    "action": "buzz",
+    "user_id: a1a4dc35-c81b-4d35-8aca-0600a9f95e02,
+    "room_id: "ABCDE
+}
+"""
+
+dynamodb = boto3.resource("dynamodb")
+buzzes_table = dynamodb.Table("buzzes")
+connections_table = dynamodb.Table("connections")
 
 def lambda_handler(event, context):
+    # ---------- Lambda Logistics ----------
+    print(event)
+
+    #establish variables from request
     connection_id = event["requestContext"]["connectionId"]
     domain = event["requestContext"]["domainName"]
     stage = event["requestContext"]["stage"]
     endpoint_url = f"https://{domain}/{stage}"
 
+    #establish connection to the client
     client = boto3.client("apigatewaymanagementapi", endpoint_url=endpoint_url)
 
-    response_data = {
-        "message": "You have buzzed (test).",
-        "status": "success"
-    }
+    #sends msg (JSON) back to client
+    def send_to_client(msg, c_id):
+        client.post_to_connection(ConnectionId=c_id, Data=json.dumps(msg))
 
-    client.post_to_connection(
-        ConnectionId=connection_id,
-        Data=json.dumps(response_data)
+    #returns true if this buzz was first in the buzzes table (requires a list of all dynamodb buzz items, and the current buzz time)
+    def was_first_buzz(buzzes, cur_buzz: str):
+        if (not buzzes):
+            return True
+
+        for buzz in buzzes:
+            if buzz["buzz_time"].fromisoformat() < cur_buzz.fromisoformat():
+                return False
+
+        return True
+
+    # ---------- Actual Lambda Logic ----------
+
+    #add buzz item into "buzzes" table
+    body = json.loads(event["body"])
+    room_id = body["room_id"]
+    user_id = body["user_id"]
+    buzz_time = datetime.now(timezone.utc)
+    put_response = buzzes_table.put_item(
+        Item = {
+            "room_id": room_id,
+            "user_id": user_id,
+            "buzz_time": buzz_time
+        }
     )
 
-    return {"statusCode": 200,}
+    #check if the buzz was first in the room
+    query_response = buzzes_table.query(KeyConditionExpression=Key("room_id").eq(room_id))
+    items = query_response.get("Items", [])
+
+    if was_first_buzz(items, buzz_time):
+        #get a list of all connection_ids in the current room
+        query_response2 = connections_table.query(KeyConditionExpression=Key("room_id").eq(room_id))
+        items2 = query_response2.get("Items", [])
+
+        connection_ids = []
+        for connection in items2:
+            if (connection["connection_id"] != connection_id):
+                connection_ids.append(connection["connection_id"])
+
+        #send message to all connections for that room
+        for c in connection_ids:
+            send_to_client({
+                "message": f"{user_id} buzzed first",
+                "status": "success"
+            }, c)
+
+
+    send_to_client({
+        "message": "You didn't buzz first in the room.",
+        "status": "success"
+    }, connection_id)
+
+    return {"statusCode": 200}
