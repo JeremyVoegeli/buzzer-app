381239c69e2b60e3266a54e9cb2f65455d88b545
Jeremy Voegeli <jeremy.voegeli@uconn.edu>
2026-08-17 14:06:59 -0400

    updating clear_buzzes policy

--- terraform/clear_buzzes_policies.tf
+++ terraform/clear_buzzes_policies.tf
@@ -11,6 +11,12 @@ data "aws_iam_policy_document" "dynamodb_clear_buzzes_permissions" {
         actions = ["dynamodb:Query", "dynamodb:BatchWriteItem"]
         resources = [aws_dynamodb_table.buzz_in.arn]
     }
+
+    statement {
+        effect = "Allow"
+        actions = ["dynamodb:Query"]
+        resources = [aws_dynamodb_table.connected_users.arn]
+    }
 }
 
 resource "aws_iam_policy" "clear_buzzes_dynamodb_policy" {
@@ -21,6 +27,25 @@ resource "aws_iam_policy" "clear_buzzes_dynamodb_policy" {
 resource "aws_iam_role_policy_attachment" "clear_buzzes_dynamodb_policy_attachment" {
     role = aws_iam_role.clear_buzzes_role.name
     policy_arn = aws_iam_policy.clear_buzzes_dynamodb_policy.arn
+}
+
+# ---------- API Call Permissions ----------
+data "aws_iam_policy_document" "api_clear_buzzes_permissions" {
+    statement {
+        effect = "Allow"
+        actions = ["execute-api:ManageConnections"]
+        resources = ["${aws_apigatewayv2_api.websocket_api.execution_arn}/prod/GET/@connections/*"]
+    }
+}
+
+resource "aws_iam_policy" "clear_buzzes_api_policy" {
+    name = "api_call_policies"
+    policy = data.aws_iam_policy_document.api_clear_buzzes_permissions.json
+}
+
+resource "aws_iam_role_policy_attachment" "clear_buzzes_api_policy_attachment" {
+    role = aws_iam_role.clear_buzzes_role.name
+    policy_arn = aws_iam_policy.clear_buzzes_api_policy.arn
 }
 
 # ----------CloudWatch Logging Policy ----------
